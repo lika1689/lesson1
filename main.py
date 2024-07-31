02214fb639b195fc98a082825de1eccaea09fb50
lika1689 <lika.zaal10@gmail.com>
2024-07-31 19:01:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+str = 'это моя строка'
+print(1, str[0])
+print(2, str[-1])
+print(3, str[2:5])
+print(4, str[::-1])
+print(5, len(str))
+print(6, 'это новая строка ' + str)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
